06681e76e668e574d8b8087d4fe0433370f2d6b3
Tej313 <tejbro02@gmail.com>
2023-12-24 21:38:54 +0545

    multiply

---
+++ multiply.py
@@ -0,0 +1,7 @@
+def multiply(x, y):
+    return x * y
+
+# Example usage for Multiplication
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+print(f"Multiplication: {multiply(num1, num2)}")
