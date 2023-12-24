017542737f976df784863b506e54c7f84ffdaf53
Tej313 <tejbro02@gmail.com>
2023-12-24 21:34:41 +0545

    subtract

---
+++ add.py
@@ -0,0 +1,7 @@
+def add(x, y):
+    return x + y
+
+# Example usage for Addition
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+print(f"Addition: {add(num1, num2)}")
---
+++ subtract.py
@@ -0,0 +1,7 @@
+def subtract(x, y):
+    return x - y
+
+# Example usage for Subtraction
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+print(f"Subtraction: {subtract(num1, num2)}")
